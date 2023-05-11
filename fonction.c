0e6998ed87ef28e0c18d999d2323c9048a1f8af6
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-11 11:20:58 +0200

    fonction.c

---
+++ fonction.c
@@ -0,0 +1,13 @@
+void creer_plateau(){
+    int i=0,j=0;
+    for(i;i<6;i++){
+        for(j;j<6;j++){
+            
+        }
+    }
+    for(i;i<6;i++){
+        for(j;j<6;j++){
+            
+        }
+    }
+}
