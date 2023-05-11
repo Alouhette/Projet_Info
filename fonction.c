1df4cc53673bd3565a6edaef584dd048a26ecbc8
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-11 11:37:23 +0200

    fonction.c

--- fonction.c
+++ fonction.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 #include <bool.h>
 
-void creer_plateau(){
+
+int case_interdite(case plateau[7][7], int x, int y ){
+    
+    if(x==) // case départ 
+    {return 0;}
+    
+    if(plateau [x][y].tresor !=0 &&  plateau [x][y].monstre!=0 ...){
+        return 0;
+        
+    }return 1;
+       
+     
+        
+    
+}
+
+void creer_plateau(plateau){
     int i=0,j=0;
     for(i;i<6;i++){
         for(j;j<6;j++){
             //creer les elem case (caché et sans monstre tresor et arme)
         }
     }
-    for(i;i<6;i++){
-        for(j;j<6;j++){
-            //creer les case avec monstre ou tresor ou arme.
-        }
-    }
+   int x, y;
+    
+  x=rand()%7;
+  y=rand()%7;
+  while(case_interdite(plateau[x][y]){ 
+       //...
+   }
+  plateau[x][y].tresor=1;
+    for (int 1=1; i<5;
+         
+    
 }
