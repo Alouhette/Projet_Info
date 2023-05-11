947af39b2be9c1d04e596483ccd3501fa4ac8afd
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-11 11:23:06 +0200

    fonction.c

--- fonction.c
+++ fonction.c
@@ -2,12 +2,12 @@ void creer_plateau(){
     int i=0,j=0;
     for(i;i<6;i++){
         for(j;j<6;j++){
-            
+            //creer les elem case (caché et sans monstre tresor et arme)
         }
     }
     for(i;i<6;i++){
         for(j;j<6;j++){
-            
+            //creer les case avec monstre ou tresor ou arme.
         }
     }
 }
