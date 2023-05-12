3433c29b81020b706d1db4770f7e07cfb9d86c00
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-12 12:40:59 +0200

    donnee.c

--- donnee.c
+++ donnee.c
@@ -20,5 +20,7 @@ typedef struct{
     bool cache; // (1)== cache , (0)== visible
     char monstre; // 0 pas de monstre , 1,2,3,4 monstre associé
     char relique; // 0 pas relique , 1,2,3,4 relique associé
+    char portail; // 1 = portail , 0= pas de portail
+    char totem; // 0 pas de totem , 1 = totem n'1 , 2 = totem n'2
     bool tresor; // (0) pas de tresor , (1) tresor present.
 }Case;
