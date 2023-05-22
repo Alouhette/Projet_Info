d86118b4afefef07579bb2244712b416e91dad0a
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-22 08:41:22 +0200

    Update execution.c

--- execution.c
+++ execution.c
@@ -8,19 +8,40 @@ int lancer_jeu(){
   return v;
 }
 
-int jeu(int nb_j){
+int jeu(int nb_j,Joueurs tab_j){
   int vainqueur=0;
   while(vainqueur == 0){
     for(int i=0;i<nb_j;i++){
       int en_vie = 1;
-      while(en_vie==1){
-        case_adjacent(); /verif si le joueur peut bouger
+      int bloquer = 0;
+      while(en_vie==1 && bloquer = 0){
+        destination = 0;
         choix_arme();    /choix de l'arme
-        deplacement();   /fct de mouvement du joueurs
+        destination = deplacement();   /fct de mouvement du joueurs
+        if(destination==2){
+          xt=(*x)-1;
+          yt = y:
+        }
+        if(destination==4){
+          yt=(*y)-1;
+          xt = x;
+        }
+        if(destination==6){
+          yt=(*y)+1;
+          xt = x;
+        }
+        if(destination==8){
+          xt=(*x)+1;
+          yt = y;
+        }
+        if(xt==0 || yt == 0 || xt==6 || yt==6){
+            en_vie = 0
+        }
         effet_case();    /effet de la case de destination
         victoire();      /test condition victoire
-        
-    }
+        case_adjacent(); /verif si le joueur peut bouger        
+      }
+    
   }
 }
 */
