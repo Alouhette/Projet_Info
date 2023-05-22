73cfe6d2deb974b42301d5ef5de450657ad8e195
KHEFY <133869969+KHEFY@users.noreply.github.com>
2023-05-22 10:35:26 +0200

    Update and rename Proto.h to proto.h

--- proto.h
+++ proto.h
@@ -5,3 +5,6 @@ int case_interdite(case plateau[7][7], int x, int y );
 int deplacement();
 int choix_arme(Joueurs j);
 void creer_plateau(case plateau[7][7]);
+void affiche_titre();
+int lancer_jeu();
+int jeu(int nb_j,Joueurs tab_j);
