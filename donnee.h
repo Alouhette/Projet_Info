43d97daf4124fcafb4d9d5f48eb368aa3bd41ad6
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-23 13:05:29 +0200

    Update donnee.h

--- donnee.h
+++ donnee.h
@@ -4,18 +4,18 @@
 #include <bool.h>
 
 typedef struct{
-    char perso; //perso choisi
-    int relique; //val pour indiquer si il a ou non la relique 0 = non obtenu
-    char arme; //arme equipe pour ce tour
+    char character; //perso choisi
+    int relic; //val pour indiquer si il a ou non la relique 0 = non obtenu
+    char weapon; //arme equipe pour ce tour
     int indice_x; 
     int indice_y;
-}Joueurs;
+}Player;
 
 typedef struct{
-    bool cache; // (1)== cache , (0)== visible
-    char monstre; // 0 pas de monstre , 1,2,3,4 monstre associé
-    char relique; // 0 pas relique , 1,2,3,4 relique associé
-    char portail; // 1 = portail , 0= pas de portail
+    bool hidden; // (1)== cache , (0)== visible
+    char monster; // 0 pas de monstre , 1,2,3,4 monstre associé
+    char relic; // 0 pas relique , 1,2,3,4 relique associé
+    char portal; // 1 = portail , 0= pas de portail
     char totem; // 0 pas de totem , 1 = totem n'1 , 2 = totem n'2
-    bool tresor; // (0) pas de tresor , (1) tresor present.
-}Case;
+    bool treasure; // (0) pas de tresor , (1) tresor present.
+}Space;
