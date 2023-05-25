da7c2eef2e7d9731432a36e59f5f9aa6a59a81c0
KHEFY <133869969+KHEFY@users.noreply.github.com>
2023-05-25 10:39:21 +0200

    Update proto.h

--- proto.h
+++ proto.h
@@ -1,10 +1,9 @@
 #include "fonction.c"
 #include "donnee.h"
 #include <stdio.h>
-int case_interdite(case plateau[7][7], int x, int y );
-int deplacement();
-int choix_arme(Joueurs j);
-void creer_plateau(case plateau[7][7]);
-void affiche_titre();
-int lancer_jeu();
-int jeu(int nb_j,Joueurs tab_j);
+int forbidden_space(Space board[7][7], int x, int y );
+int movement();
+int weapon_choice();
+int victory(Player j,int num_j);
+int other_Space(Space board[7][7],int x,int y);
+void create_board(Space board[7][7]);
