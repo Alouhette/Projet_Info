b97281f3d97dd29c05766bc5325be0f1aea03100
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-26 17:51:11 +0200

    Update test.c

--- test.c
+++ test.c
@@ -39,6 +39,32 @@ void create_board(Space board[7][7]){
        board[y][x].totem=1;
    }
    for(i=1; i<5;i++){
+      int comp1=0;
+      while((forbidden_space(board,x,y))&&(comp2<5)){ 
+           int comp2=0;
+           if(y<5){
+               y++;
+           }
+           else{
+               y=1;
+           }
+           if(forbidden_space(board,x,y)){
+                while((forbidden_space(board,x,y))&&(comp2<5)){ 
+                    if(x<5){
+                       x++;
+                    }
+                    else{
+                       x=1;
+                    }
+                    comp2++;
+                }
+            }
+            comp1++;            
+            }
+        board[y][x].relic=i;
+      }
+   }
+   for(i=1; i<5;i++){
         for(j=1 ; j<5 ; j++){
             int comp1=0;
             while((forbidden_space(board,x,y))&&(comp2<5)){ 
