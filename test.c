46b9b4e3a111e74be852b5ae35b60d0195d13fa0
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-26 17:38:17 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,14 +1,6 @@
-void create_board(Space board[7][7]){
-    int i=0,j=0;
-    for(i;i<6;i++){
-        for(j;j<6;j++){
-            //creer les elem case (caché et sans monstre tresor et arme)
-            Space tile;
-            tile.hidden = 1;
-            tile.monster = 0;
-            tile.relic = 0;
-            tile.treasure = 0;
-            board[j][i] = tile;            
-        }
-    }
-    int x, y;    
+int forbidden_space(Space board[7][7], int x, int y ){ 
+    if(board[x][y].treasure !=0 &&  board[x][y].monster!=0 && board[x][y].relic!=0){
+        return 1;   
+    };
+    return 0;   
+}
