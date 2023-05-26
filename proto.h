b6c27f02ffcc58612e2b585afdcd2d22ece7c635
KHEFY <133869969+KHEFY@users.noreply.github.com>
2023-05-26 12:12:14 +0200

    Update proto.h

--- proto.h
+++ proto.h
@@ -14,3 +14,8 @@ int space_effect(Space x, Player user);
 void show_symbol(Space x);
 int victory(Player j,int num_j);
 int other_Space(Space board[7][7],int x,int y);
+void title();
+void tab_score(Player tab[], int nb_j);
+void show_symbol(Space x);
+void show_board(Space board[7][7]);
+void show_availability(char character_available[4]);
