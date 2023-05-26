69e36eea14c3da025d71a28a81d9a3678fe293e5
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-26 17:57:12 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -25,26 +25,7 @@ void create_board(Space board[7][7]){
         y=1 + rand()%5;
    }
    board[y][x].treasure=1;
-   for (i=1; i<5;i++){
-        for(j=1 ; j<5 ; j++){
-            while(forbidden_space(board,x,y)){ 
-                x=1 + rand()%5;
-                y=1 + rand()%5;
-            }
-            board[y][x].monster=i;
-        }
-   }
-   x=1 + rand()%5;
-   y=1 + rand()%5;
-   for(i=1 ; i<5 ; j++){
-       x=1 + rand()%5;
-       y=1 + rand()%5;
-       while(forbidden_space(board,x,y)){ 
-             x=1 + rand()%5;
-             y=1 + rand()%5;
-       }
-       board[y][x].relic=i;
-   }
+   int x, y;    
    x=1 + rand()%5;
    y=1 + rand()%5;
    while(forbidden_space(board,x,y)){ 
@@ -63,7 +44,59 @@ void create_board(Space board[7][7]){
        }
        board[y][x].totem=1;
    }
-}
+   for(i=1; i<5;i++){
+      int comp1=0;
+      while((forbidden_space(board,x,y))&&(comp2<5)){ 
+           int comp2=0;
+           if(y<5){
+               y++;
+           }
+           else{
+               y=1;
+           }
+           if(forbidden_space(board,x,y)){
+                while((forbidden_space(board,x,y))&&(comp2<5)){ 
+                    if(x<5){
+                       x++;
+                    }
+                    else{
+                       x=1;
+                    }
+                    comp2++;
+                }
+            }
+            comp1++;            
+            }
+        board[y][x].relic=i;
+      }
+   }
+   for(i=1; i<5;i++){
+        for(j=1 ; j<5 ; j++){
+            int comp1=0;
+            while((forbidden_space(board,x,y))&&(comp2<5)){ 
+                int comp2=0;
+                if(y<5){
+                    y++;
+                }
+                else{
+                    y=1;
+                }
+                if(forbidden_space(board,x,y)){
+                     while((forbidden_space(board,x,y))&&(comp2<5)){ 
+                        if(x<5){
+                            x++;
+                        }
+                        else{
+                            x=1;
+                        }
+                        comp2++;
+                    }
+                }
+                comp1++;            
+          }
+          board[y][x].monster=i;
+        }
+   }
 
 //int character_available[4] //indice 0 = mage, 1 = guerrier, 2 = ranger, 3 = voleur
 Player create_player(int *character_available){
