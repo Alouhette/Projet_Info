250cabacf6471cc7b00084832779bc349e59b6af
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-26 15:05:59 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -1,8 +1,8 @@
 int forbidden_space(Space board[7][7], int x, int y ){ 
     if(board[x][y].treasure !=0 &&  board[x][y].monster!=0 && board[x][y].relic!=0){
-        return 0;   
+        return 1;   
     };
-    return 1;   
+    return 0;   
 }
 
 void create_board(Space board[7][7]){
