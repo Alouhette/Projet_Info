445682c08da338cc71c13efa63343ae915c569ef
Happy0503 <90928071+Happy0503@users.noreply.github.com>
2023-05-26 14:20:50 +0200

    Update verification.h

--- verification.h
+++ verification.h
@@ -6,7 +6,7 @@ int type_int(){
   float n1;
   int verif;
   do{
-    verif = scanf("%df",&n1);
+    verif = scanf("%d",&n1);
     clear_buffer();
   }while(n1>4 || n1<0 || verif!=1); //Pour des saisies entre 0 et 4
 return n1;
@@ -15,7 +15,7 @@ int type_coord(){
   float n1;
   int verif;
   do{
-    verif = scanf("%df",&n1);
+    verif = scanf("%d",&n1);
     clear_buffer();
   }while(n1>5 || n1<1 || verif!=1); //Pour des saisies entre 1 et 5
 return n1;
@@ -26,7 +26,7 @@ int type_dest(){
   int verif2=0;
   do{
     verif2 = 0;
-    verif = scanf("%df",&n1);
+    verif = scanf("%d",&n1);
     clear_buffer();
     if (n1==2 || n1==4 || n1==6 || n1==8){
       verif2=1;
