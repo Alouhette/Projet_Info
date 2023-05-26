998f9eadaccf302bd7bd903e554bf9499d11dbd4
KHEFY <133869969+KHEFY@users.noreply.github.com>
2023-05-26 12:22:39 +0200

    Update proto.h

--- proto.h
+++ proto.h
@@ -1,7 +1,15 @@
-#include "fonction.h"
 #include "donnee.h"
+#include "verification.h"
 #include "display.h"
-#include <stdio.h>
+#include "fonction.h"
+void clear_buffer();
+int type_int();
+int type_coord();
+void title();
+void tab_score(Player tab[], int nb_j);
+void show_symbol(Space x);
+void show_board(Space board[7][7]);
+void show_availability(char character_available[4]);
 void create_board(Space board[7][7]);
 int forbidden_space(Space board[7][7], int x, int y );
 Joueur create_player(char character_available[4]);
@@ -14,11 +22,4 @@ int space_effect(Space x, Player user);
 void show_symbol(Space x);
 int victory(Player j,int num_j);
 int other_Space(Space board[7][7],int x,int y);
-void clear_buffer();
-int type_int();
-int type_coord();
-void title();
-void tab_score(Player tab[], int nb_j);
-void show_symbol(Space x);
-void show_board(Space board[7][7]);
-void show_availability(char character_available[4]);
+
