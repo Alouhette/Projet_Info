437bc62975b34c21ed35fae797f92850f22b3ddb
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-26 13:55:11 +0200

    Update execution.c

--- execution.c
+++ execution.c
@@ -69,7 +69,7 @@ int game(int nb_j,Player *tab_j,Space **board){
 
 int play_game(){
   int player;
-  int character_tab[4]={1};
+  Player character_tab[4]={1};
   printf("┬ ┬┌─┐┬ ┬  ┌┬┐┌─┐┌┐┌┬ ┬  ┌─┐┬  ┌─┐┬ ┬┌─┐┬─┐  ┌─┐\n");
   printf("├─┤│ ││││  │││├─┤│││└┬┘  ├─┘│  ├─┤└┬┘├┤ ├┬┘   ┌┘\n");
   printf("┴ ┴└─┘└┴┘  ┴ ┴┴ ┴┘└┘ ┴   ┴  ┴─┘┴ ┴ ┴ └─┘┴└─   o \n");
