29dcff2fd41efe13c06222d07fdeb89a2e32e7aa
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-26 23:01:42 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,3 +1,39 @@
+void e(int board[7][7],int *x,int *y){
+    while(board[*y][*x]!=0){
+        if(*x<5){
+           *x++;
+        }
+        else{
+            *x=1;
+            *y++;
+        }
+        if(*y>5){
+            *y=1;
+        }
+    }
+}    
+
+int main()
+{
+    srand(time(NULL));
+    int tab[7][7];
+    int i=0,j=0;
+    for(i;i<6;i++){
+        for(j;j<6;j++){
+            tab[j][i]==0;
+        }
+    }
+    int* x, * y;
+    *x=1 + rand()%5;
+    *y=1 + rand()%5;
+    tab[*y][*x]==1;
+    for(i=1;i<4;i++){
+        if(tab[*y][*x]!=0){
+            e(tab,x,y);
+        } 
+        tab[*y][*x]==i;
+    }
+}
 
 //ancien
 void create_board(Space board[7][7]){
