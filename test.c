6c7deffda1cb17ca87d5f1f9a35b811c19ad99c6
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-26 17:45:36 +0200

    Update test.c

--- test.c
+++ test.c
@@ -21,15 +21,29 @@ void create_board(Space board[7][7]){
    board[y][x].treasure=1;
    for (i=1; i<5;i++){
         for(j=1 ; j<5 ; j++){
-            while(forbidden_space(board,x,y)){ 
+            int comp1=0;
+            while(forbidden_space(board,x,y)&&(comp2<5)){ 
+                int comp2=0;
                 if(x<5){
                     x++;
                 }
                 else{
                     x=1;
                 }
-            }
-            board[y][x].monster=i;
+                if(forbidden_space(board,x,y)){
+                     while(forbidden_space(board,x,y)&&(comp2<5)){ 
+                        if(y<5){
+                            y++;
+                        }
+                        else{
+                            y=1;
+                        }
+                        comp2++;
+                    }
+                }
+                comp1++;            
+          }
+          board[y][x].monster=i;
         }
    }
   
