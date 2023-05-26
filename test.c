ed461de603ea87a9740a3bc954bb1e8839aea2eb
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-26 17:55:37 +0200

    Update test.c

--- test.c
+++ test.c
@@ -11,19 +11,6 @@ void create_board(Space board[7][7]){
             board[j][i] = tile;            
         }
     }
-    void create_board(Space board[7][7]){
-    int i=0,j=0;
-    for(i;i<6;i++){
-        for(j;j<6;j++){
-            //creer les elem case (caché et sans monstre tresor et arme)
-            Space tile;
-            tile.hidden = 1;
-            tile.monster = 0;
-            tile.relic = 0;
-            tile.treasure = 0;
-            board[j][i] = tile;            
-        }
-    }
    int x, y;    
    x=1 + rand()%5;
    y=1 + rand()%5;
