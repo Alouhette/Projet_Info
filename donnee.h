21c1d68e5320ccad0c9f37763fce4d124c887ac9
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-27 17:09:58 +0200

    Update donnee.h

--- donnee.h
+++ donnee.h
@@ -21,10 +21,10 @@ typedef struct{
 
 typedef struct{
     int hidden; // (1)== cache , (0)== visible
-    char monster; // 0 pas de monstre , 1,2,3,4 pour un monstre spécifique
-    char relic; // 0 pas relique , 1,2,3,4 pour une relique spécifique
-    char portal; // 1 = portail , 0 = pas de portail
-    char totem; // 1 = totem , 0 = pas de totem
+    int monster; // 0 pas de monstre , 1,2,3,4 pour un monstre spécifique
+    int relic; // 0 pas relique , 1,2,3,4 pour une relique spécifique
+    int portal; // 1 = portail , 0 = pas de portail
+    int totem; // 1 = totem , 0 = pas de totem
     int treasure; // 1 = tresor , 0 = pas de tresor
     int cleared; //1 = case faite, 0 = pas faite
 }Space;
