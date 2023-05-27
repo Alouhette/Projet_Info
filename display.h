cb772088aa9112875d735c1a27c214a6b9d13c2a
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-27 21:18:04 +0200

    Update display.h

--- display.h
+++ display.h
@@ -76,8 +76,17 @@ void show_symbol(Space x){
 void show_board(Space board[7][7]){
     for (int i=0;i<7;i++){
         for (int j=0;j<7;j++){
-            if ((j==4&&i==0)||(j==0&&i==2)||(j==6&&i==4)||(j==2&&i==6)){
-                printf(" d ");
+            if (j==4&&i==0){
+               printf(ANSI_COLOR_GREEN   "d"   ANSI_COLOR_RESET);
+            }
+            else if (j==0&&i==2){
+               printf(ANSI_COLOR_BLUE " d " ANSI_COLOR_RESET);
+            }
+            else if(j==6&&i==4){
+               printf(ANSI_COLOR_MAGENTA " d " ANSI_COLOR_RESET);
+            }
+            else if(j==2&&i==6){
+               printf(ANSI_COLOR_YELLOW  " d "  ANSI_COLOR_RESET);
             }
             else{
                 if((j<1||j>5)||(i<1||i>5)){
