a37f2a82e16315487e35325520e0a78324aaa347
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-27 18:49:12 +0200

    Update display.h

--- display.h
+++ display.h
@@ -77,7 +77,7 @@ void show_board(Space board[7][7]){
     for (int i=0;i<7;i++){
         for (int j=0;j<7;j++){
             if ((j==4&&i==0)||(j==0&&i==2)||(j==6&&i==4)||(j==2&&i==6)){
-                printf(" D ");
+                printf(" ▨ ");
             }
             else{
                 if((j<1||j>5)||(i<1||i>5)){
