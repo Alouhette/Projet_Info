57b7f6d0264a5990ebf57e194ef8dbbb46001384
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-27 23:16:21 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -10,10 +10,10 @@ void adapt_indice(Space board[7][7],int *x,int *y){
         if(*x<5){
             *x++;
         }
-        if(*x==5){
+        else if(*x==5){
             *x=1;
             *y++;
-        }
+        };
         if(*y>5){
             *y=1;
         };
