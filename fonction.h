1da242532da5d650265b1f4fb1ffc52dcf3d64c1
Alouhette <133207604+Alouhette@users.noreply.github.com>
2023-05-28 13:08:59 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -21,8 +21,8 @@ void adapt_indice(Space board[7][7], int *x, int *y) {
 
 void create_board(Space board[7][7]){
     int i,j;
-    for(i=0;i<7;i++){
-        for(j=0;j<7;j++){
+    for(i=1;i<6;i++){
+        for(j=1;j<6;j++){
             //creer les elem case (caché et sans monstre tresor et arme)
             Space tile;
             tile.hidden = 1;
